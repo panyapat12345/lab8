60e22fd77d2190829e1d69e9e530d84183462e3a
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-03 17:14:02 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	if(x < 'A' || x > 'Z'){
+		return '0';
+	}else{
+
+		if(x == 'A'){
+			return 'Z';
+		}else{
+			return x - 1;
+		}
+
+	}
 }
 
 int main(){
